351f43364baed954fa09584c506b67ac4bb36af7
Phiremouse <Byford.Boyd@gmail.com>
2019-11-05 18:34:12 -0600

    fix 2

--- static/js/bonus.js
+++ static/js/bonus.js
@@ -4,37 +4,37 @@ function buildGauge(washfreq){
     showlegend: false,
     hole: 0.3,
     rotation: 90,
-    values: [100 / 5, 100 / 5, 100 / 5, 100 / 5, 100 / 5, 100],
-    text: ["Very Low", "Low", "Average", "Good", "Excellent", ""],
+    values: [100 / 9, 100 / 9, 100 / 9, 100 / 9, 100 / 9,100 / 9,100 / 9,100 / 9,100 / 9, 100],
+    text: ["0-1", "1-2", "2-3", "3-4", "4-5","5-6","6-7","7-8","8-9", ""],
     direction: "clockwise",
     textinfo: "text",
     textposition: "inside",
     marker: {
       colors: ["rgba(255, 0, 0, 0.6)", "rgba(255, 165, 0, 0.6)", "rgba(255, 255, 0, 0.6)", "rgba(144, 238, 144, 0.6)", "rgba(154, 205, 50, 0.6)", "rgba(154, 350, 125, 0.6)"]
     },
-    labels: ["0-10", "10-50", "50-200", "200-500", "500-2000", ""],
+    labels: ["0-1", "1-2", "2-3", "3-4", "4-5","5-6","6-7","7-8","8-9", ""],
     hoverinfo: "label"
   };
   
-  var degrees = 69;
+  var degrees = (180/9) * washfreq+10;
   var radius = .5;
   var radians = degrees * Math.PI/180;
-  var x = Math.cos(radians);
+  var x = 1 * radius * Math.cos(radians);
   var y = Math.sin(radians);
   
   var layout = {
     shapes:[{
         type: 'line',
-        x0: 1,
+        x0: .5,
         y0: .5,
-        x1: 1,
-        y1: y,
+        x1: x,
+        y1: .5,
         line: {
           color: 'black',
           width: 8
         }
       }],
-    title: 'Number of Printers Sold in a Week',
+    title: 'Frequency of Washing',
     xaxis: {visible: false, range: [-1, 1]},
     yaxis: {visible: false, range: [-1, 1]}
   };
